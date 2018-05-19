055b5c62ea113a64ea8c30999b7f9c41106596c4
Stavros Kampanakis <stavros.kampanakis@gmail.com>
2018-05-19 19:04:58 +0300

    added eslint

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,29 +1,27 @@
-import React from 'react';
+import React from "react";
 import Color from "color";
 import styled from "styled-components";
-import { Button as BootstrapButton } from 'reactstrap';
+import { Button as BootstrapButton } from "reactstrap";
 
-const mainColor = Color('rebeccapurple');
+const mainColor = Color("rebeccapurple");
 
 const StyledButton = styled(BootstrapButton)`
-    background-color: ${mainColor.hex()}!important;
-    border-color: ${mainColor.hex()}!important;
-    color: #FFFF;
-    &:hover {
-        background-color: ${mainColor.darken(0.15).hex()}!important;
-    }
-    &:focus {
-        box-shadow: 0 0 0 0.2rem ${mainColor.lighten(0.75).hex()}!important;
-    }
-    &:active {
-        background-color: ${mainColor.darken(0.20).hex()}!important;
-    }
+  background-color: ${mainColor.hex()}!important;
+  border-color: ${mainColor.hex()}!important;
+  color: #ffff;
+  &:hover {
+    background-color: ${mainColor.darken(0.15).hex()}!important;
+  }
+  &:focus {
+    box-shadow: 0 0 0 0.2rem ${mainColor.lighten(0.75).hex()}!important;
+  }
+  &:active {
+    background-color: ${mainColor.darken(0.2).hex()}!important;
+  }
 `;
 
 const Button = () => {
-    return (
-        <StyledButton>The Custom Button</StyledButton>
-    )
+  return <StyledButton>The Custom Button</StyledButton>;
 };
 
 export default Button;
