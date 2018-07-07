900177415b9d298d7c829a3e6689b32e17678ede
Stavros Kampanakis <stavros.kampanakis@gmail.com>
2018-07-07 13:52:25 +0300

    style changes to marker popup

--- src/components/MarkerPopup/MarkerPopup.js
+++ src/components/MarkerPopup/MarkerPopup.js
@@ -1,22 +1,41 @@
 import React from "react";
+import moment from "moment";
 import PropTypes from "prop-types";
 import { Table } from "reactstrap";
 import { StyledMarkerPopup } from "./MarkerPopup.styles";
 
 const MarkerPopup = props => {
   const { info } = props;
-  const { properties } = info;
+  const { properties, geometry } = info;
   return (
     <StyledMarkerPopup>
+      <h6>{properties.title}</h6>
       <Table striped>
         <tbody>
           <tr>
             <th scope="row">Time:</th>
-            <td>{properties.time}</td>
+            <td>
+              {moment(properties.time).format("MMMM Do YYYY, h:mm:ss a")}
+              {" (UTC)"}
+            </td>
           </tr>
           <tr>
             <th scope="row">Magnitude:</th>
-            <td>{properties.mag}</td>
+            <td>
+              {properties.mag} {properties.magType}
+            </td>
+          </tr>
+          <tr>
+            <th scope="row">Longitude:</th>
+            <td>{geometry.coordinates[0]} degrees</td>
+          </tr>
+          <tr>
+            <th scope="row">Latitude:</th>
+            <td>{geometry.coordinates[1]} degrees</td>
+          </tr>
+          <tr>
+            <th scope="row">Depth:</th>
+            <td>{geometry.coordinates[2]} km</td>
           </tr>
           <tr>
             <th scope="row">Tsunami Warning:</th>
@@ -24,6 +43,9 @@ const MarkerPopup = props => {
           </tr>
         </tbody>
       </Table>
+      <a target="_blank" rel="noopener noreferrer" href={properties.url}>
+        More info here
+      </a>
     </StyledMarkerPopup>
   );
 };
